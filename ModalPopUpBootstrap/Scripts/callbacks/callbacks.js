1d1302801d181a2d7f28330fd485d4346bc60d9e
jedielson <jedielson.jn@outlook.com>
2015-10-16 14:54:19 -0300

    Arquivo com os callbacks de exemplo

---
+++ ModalPopUpBootstrap/Scripts/callbacks/callbacks.js
@@ -0,0 +1,12 @@
+﻿
+function tratamentoCallBack1(data) {
+
+    console.log('Entrou no CallBack1...');
+    console.log(data);
+}
+
+function tratamentoCallBack2(data) {
+
+    console.log('Entrou no CallBack2...');
+    console.log(data);
+}
